39c0d3e385883ea1a5615ebfc685e86ef4b9758b
Daniel-Garrido <danielarcangelgh@gmail.com>
2025-10-01 01:06:08 -0600

    cambios a la pagina

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,21 +8,21 @@
     <div class="container footer-top pb-5">
       <div class="row g-4">
         <!-- contenedor contacto -->
-        <div class="col-12 col-lg-4">
+        <div class="col-12 col-lg-4"   data-aos="fade-up">
           <h6 class="ft-title mb-3">Contactame</h6>
           <p class="ft-big mb-2"> 999 638 3996</p>
           <p class="ft-big mb-0">mersigarridohoil@gmail.com</p>
         </div>
 
         <!-- Contenedor direccion -->
-        <div class="col-12 col-lg-4">
+        <div class="col-12 col-lg-4"   data-aos="fade-up">
           <h6 class="ft-title mb-3">Mi dirección</h6>
           <p class="ft-big mb-2">Calle 19 x 18 y 20 </p>
           <p class="ft-big mb-0">Cuzamá, Yucatán</p>
         </div>
 
         <!-- Contenedor horario de trabajo -->
-        <div class="col-12 col-lg-4">
+        <div class="col-12 col-lg-4"   data-aos="fade-up">
           <h6 class="ft-title mb-3">Horario</h6>
           <p class="ft-big mb-2"><em>Lunes a viernes :</em> 08.00 am – 9.00 pm</p>
           <p class="ft-big mb-0"><em>Sábado:</em> 11:00 am - 7:00 pm</p>
@@ -36,13 +36,13 @@
     <!-- Bottom bar -->
     <div class="container footer-bottom py-4">
       <div class="row align-items-center">
-        <div class="col-12 col-md-6 text-center text-md-start mb-3 mb-md-0">
+        <div class="col-12 col-md-6 text-center text-md-start mb-3 mb-md-0"  >
           <small> Desarrollado por &copy; Daniel Garrido</small>
         </div>
 
         <div class="col-12 col-md-6 d-flex justify-content-center justify-content-md-end align-items-center gap-3">
           <!-- redes sociales -->
-          <nav class="footer-social d-flex align-items-center gap-3 me-3">
+          <nav class="footer-social d-flex align-items-center gap-3 me-3"   >
             <a href="https://www.facebook.com/profile.php?id=100072208549016" target="_blank" aria-label="Facebook">
               <i class="fab fa-facebook"></i>
             </a>
@@ -59,7 +59,6 @@
 </template>
 
 <style>
-
 
 /* estilos a los iconos de redes sociales */
 .footer-social{
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/' //conectar a github pages
+  //base: 'https//Daniel-Garrido.github.io/LandingPageMersiMakeUp' //conectar a github pages
 })
